b432a0e9be25ba55bd02bef5e25504cfed779b9c
Alessio Pancia <alessiop2801@gmail.com>
2021-02-25 14:07:45 +0100

    links e template

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pannello di controllo</title>
-    </head>
-    <body>
-        <main>
-            <h1>Pannello di controllo amministratore</h1>
-        </main>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Pannello amministratore</h1>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,8 +24,13 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Home
                 </a>
+                @auth
+                <a class="navbar-brand" href="{{ url('/admin') }}">
+                    Pannello amministratore
+                </a>
+                @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
